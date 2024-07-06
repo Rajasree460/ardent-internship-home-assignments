d639cdcc315832dd96cd0eb76b75eb868bd321a1
Rajasree Laha <laharajasree121@gmail.com>
2024-07-06 22:08:39 +0530

    Create App_hass1.js

---
+++ essay/App_hass1.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import {Head,Unemp,Reason,Soln} from './components/Unempha1';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Head/>
+      <Unemp/>
+      <Reason/>
+      <Soln/>
+    </div>
+  );
+}
+
+
+export default App;
+
