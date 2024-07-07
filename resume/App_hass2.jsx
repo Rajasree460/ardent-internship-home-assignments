9f4deb70731947f0492ebc3880adee2a4cee943a
Rajasree Laha <laharajasree121@gmail.com>
2024-07-07 11:46:55 +0530

    Create App_hass2.jsx

---
+++ resume/App_hass2.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Headha2 from './components/Headha2';
+import Pdha2 from './components/Pdha2';
+import Eqha2 from './components/Eqha2';
+import Fdha2 from './components/Fdha2';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Headha2/>
+      <Pdha2/>
+      <Eqha2/>
+      <Fdha2/>
+    </div>
+  );
+}
+
+
+export default App;
+
