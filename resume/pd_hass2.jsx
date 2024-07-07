05be7b2c0a7b023be44b4f38c917651f29655c8f
Rajasree Laha <laharajasree121@gmail.com>
2024-07-07 11:48:43 +0530

    Create pd_hass2.jsx

---
+++ resume/pd_hass2.jsx
@@ -0,0 +1,121 @@
+//4 comp. in 4 diff files
+import React from "react";
+const Pdha2=()=>{
+    return(
+        <>
+            <h2 align="left">Personal Details</h2>
+            <img src="/myimg.jpeg" alt="myimg" style={{ width: "80px", height: "60px" }} align="left"/>
+            <hr></hr>
+            <hr></hr>
+            <p>Rajasree Laha
+            | +91 9051485696 | <a href="mailto:laharajasree121l@gmail.com">laharajasree121@gmail.com</a>  | Westbengal,India</p>
+            <hr></hr>
+            <h3 align="left">Objective</h3>
+            <hr></hr>
+            <p>
+            To excel as a Developer in a renowned IT company, leveraging professional guidance to grow while contributing to the company's success with dedication and determination.</p>
+            <hr></hr>
+            <ul>
+                <li>
+                <b><u>Coursework</u></b>
+                <p>Data Structure And Algorithms<br></br>
+                 • Operating systems<br></br>
+                 • DBMS<br></br>
+                 • Design and Analysis Algorithm<br></br>
+                 • Object oriented Programming<br></br>
+                 • Digital Logic<br></br>
+                 • Compiler Design<br></br>
+                 • Web Development<br></br>
+                 • Data Science</p>
+                </li>
+                <hr></hr>
+                <li>
+                <b><u>Skills</u></b>
+                <p>C | JavaScript | HTML/CSS |
+                MySQL | Java | OOPs | Python | R | Lex
+                | Shell programming | Bootstrap | GitHub | Git | Linux | Yacc | Microsoft Office
+                | Basic Mentorship | PL/SQL | Oracle
+                SQL | Problem Solving(Real World)</p>
+                </li>
+                <hr></hr>
+                <li>
+                <b><u>Honors</u></b>
+                <p>• Appreciated for
+                <a href="https://drive.google.com/drive/folders/14OlOYKdwZJuBFEaBwtAMj4eH5VJotAht?usp=drive_link"> Contiguous Academic excellence</a>.<br></br>
+               • <a href="https://developers.google.com/profile/badges/community/gdsc/2023/member">GDSC Member AU’23‑24</a>.<br></br>
+               • Awarded for cracking <a href="https://www.linkedin.com/in/rajasree-laha-756269254/details/honors/163814987/multiple-media-viewer/?profileId=ACoAAD6vvvIB-9deU3Im9CW76MAv4qQBAnxhH4A&treasuryMediaId=1711789023014">WBJEE</a> and
+               it’s rank,<a href="https://www.linkedin.com/in/rajasree-laha-756269254/details/honors/urn:li:fsd_profileHonor:(ACoAAD6vvvIB-9deU3Im9CW76MAv4qQBAnxhH4A,164233933)/treasury/">Madhyamik</a> and 
+               <a href="https://www.linkedin.com/in/rajasree-laha-756269254/details/honors/164189203/multiple-media-viewer/?profileId=ACoAAD6vvvIB-9deU3Im9CW76MAv4qQBAnxhH4A&treasuryMediaId=1711788209837"> HS</a> result.<br></br>
+               • Received award in <a href="https://www.linkedin.com/in/rajasree-laha-756269254/details/honors/164343997/multiple-media-viewer/?profileId=ACoAAD6vvvIB-9deU3Im9CW76MAv4qQBAnxhH4A&treasuryMediaId=1711789263840">Math Program</a>
+               and <a href="https://www.linkedin.com/in/rajasree-laha-756269254/details/honors/164343997/multiple-media-viewer/?profileId=ACoAAD6vvvIB-9deU3Im9CW76MAv4qQBAnxhH4A&treasuryMediaId=1711789263839">quiz’2016</a>.<br></br>
+               • Received many <a href="https://www.linkedin.com/in/rajasree-laha-756269254/details/honors/urn:li:fsd_profileHonor:(ACoAAD6vvvIB-9deU3Im9CW76MAv4qQBAnxhH4A,163416529)/treasury/">Academic awards </a> 
+                 till HS.<br></br></p>
+                </li>
+                <hr></hr>
+                <li>
+                <b><u>Projects</u></b>
+                <ul>
+                    <li><b>Project 1:</b> <i><a href="https://github.com/Rajasree460/Pages-Loaders_CreativeWear">CreativeWear</a></i>
+                    <p>Contributed to the Web Dev and Front End part.<br></br>
+                    Designed the Login page,SignUp Page and all Loaders.<br></br>
+                    Tech Used ‑ <b>ReactJs,HTML,CSS</b>.<br></br>
+                    </p>
+                    </li>
+                    <li><b>Project 2:</b> <i><a href="https://github.com/Rajasree460/Tic_Tac_Toe">Tic Tac Toe</a></i>
+                    <p>A common and simple game tic‑tac‑toe, where players click squares
+                    to add X or O, taking turns, avoiding already marked squares, and the
+                    game ends with a win or a draw.<br></br>
+                    Tech Used ‑ <b>JavaScript,HTML,CSS</b>.
+                    </p>
+                    </li>
+                    <li><b>Project 3:</b> <i><a href="https://github.com/Rajasree460/Netflix_Clone">Netflix Clone</a></i>
+                    <p>A simple,responsive,basic landing page on a Gym Center with pricing
+                    and compare planes section to give a more clear view.<br></br>
+                    Tech Used ‑ <b>HTML,CSS</b>.</p>
+                    </li>
+                </ul>
+                </li>
+                <hr></hr>
+                <li>
+                <b><u>Experience</u></b>
+                <ul>
+                    <li><b>Oasis Infobyte:</b> <i><a href="https://www.linkedin.com/posts/rajasree-laha-756269254_cip-oasis-infobyte-activity-7134457126474629121-r94I?utm_source=share&utm_medium=member_desktop">Web Development Intern</a></i>
+                    <p>Designed the <b>basic Landing Page</b>, a <b>Temperature Converter</b> and
+                    <b>Personal Portfolio</b> using <b>HTML,CSS</b> and <b>JavaScript</b>.
+                    </p>
+                    </li>
+                    <li><b>Bharat Intern:</b> <i><a href="https://drive.google.com/file/d/1aH8K4zxqcaGYV9i5RX6YSh8htUua6Dov/view">Web Development Intern</a></i>
+                    <p>Designed the basic <b>Netflix Homepage</b>, a <b>Temperature Converter</b> and
+                    <b>Personal Portfolio</b> using <b>HTML,CSS</b> and <b>JavaScript</b>.
+                    </p>
+                    </li>
+                    <li><i><b><a href="https://github.com/Rajasree460">Open Source Contributor</a></b></i>
+                    <p>Made over <b>923</b> total contributions and <b>444</b> total commits.
+                    </p>
+                    </li>
+                </ul>
+                </li>
+                <hr></hr>
+                <li>
+                <b><u>Achievements</u></b>
+                <p>Selected for <a href="https://www.linkedin.com/posts/rajasree-laha-756269254_internship2024-mernstackdevelopment-innovate360-activity-7208001364025626624-zhb8?utm_source=share&utm_medium=member_desktop">Innovate 360 Internship Program in XceedIQ</a>.<br></br>
+                Attempted to solve <a href="https://www.linkedin.com/posts/rajasree-laha-756269254_coding-6companies30dayz-6companies30dayz-activity-7015763514841595904-qr-g?utm_source=share&utm_medium=member_desktop">Arsh Goyal’s 6 Companies 30 days challenge Sheet</a> and
+                <a href="https://github.com/Rajasree460/6-_company-s_30_days"> solved 85 percentage</a> of questions successfully.<br></br>
+                Attended <a href="https://www.linkedin.com/feed/update/urn:li:activity:7022844359406477312/">3 days exclusive DSA Bootcamp(Workshop) in 2023</a> with 4
+                inspiring mentors.<br></br>
+                Solved <a href="https://leetcode.com/u/laharajasree121/">150+ LeetCode problems</a>.<br></br>
+                Secured <a href="https://drive.google.com/file/d/1aYE2YuwJd0AV5IUX9jCSZg7OhcVUYyaA/view">4k Rank among 180K Candidates in WBJEE 2021</a>.<br></br>
+                Consecutive <a href="https://www.linkedin.com/in/rajasree-laha-756269254/details/honors/urn:li:fsd_profileHonor:(ACoAAD6vvvIB-9deU3Im9CW76MAv4qQBAnxhH4A,163416529)/treasury/">School Grade Till 2021: EX</a>.<br></br></p>
+                </li>
+                <hr></hr>
+                <li>
+                <b><u>Languages</u></b>
+                <p>English • Bengali • Hindi</p>
+                </li>
+            </ul>
+            <hr></hr>
+        </>
+    )
+}
+
+export default Pdha2
