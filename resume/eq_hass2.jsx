09584a6936bb6888462a7a7c9d589bf06a8db94b
Rajasree Laha <laharajasree121@gmail.com>
2024-07-07 11:49:15 +0530

    Create eq_hass2.jsx

---
+++ resume/eq_hass2.jsx
@@ -0,0 +1,28 @@
+//4 comp. in 4 diff files
+import React from "react";
+const Eqha2=()=>{
+    return(
+        <>
+            <h3 align="left">Educational Qualification</h3>
+            <hr></hr>
+            <hr></hr>
+            <ul>
+            <li><b>Aliah University:</b> <i>Aug 2021 – Present</i>
+                    <p>Persuing B.Tech in Computer Science And Engineering.<br></br>Current CGPA : <a href="https://drive.google.com/file/d/163w2ScMg9jQGrnIJ65ThzzLFw_YNbvfB/view">9.56</a>
+                    </p>
+                    </li>
+                    <li><b>Batanagar Girls' High School:</b> <i>Feb 2019 – Jun 2021</i>
+                    <p> Passed HS Examination with grade of <a href="https://drive.google.com/file/d/1rrqn6w_4YtBqCbYqhYEgcZnFZbV4Xy6g/view">88.8%</a>.<br></br>
+                    </p>
+                    </li>
+                    <li><b>Batanagar Girls' High School:</b> <i>Jan 2014 – Jan 2019</i>
+                    <p> Passed Secondary Examination with grade of <a href="https://drive.google.com/file/d/1ujEaLaykjXxGjjfyb_J5xTTMPIonsocs/view">89.28%</a>.
+                    </p>
+                    </li>
+            </ul>
+            <hr></hr>
+        </>
+    )
+}
+
+export default Eqha2
