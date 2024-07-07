a41843639226fd46eb69156514df9ea0fedb58a2
Rajasree Laha <laharajasree121@gmail.com>
2024-07-07 11:49:55 +0530

    Create fd_hass2.jsx

---
+++ resume/fd_hass2.jsx
@@ -0,0 +1,14 @@
+//4 comp. in 4 diff files
+import React from "react";
+const Fdha2=()=>{
+    return(
+        <>
+            <h3 align="left">Family Details</h3>
+            <hr></hr>
+            <hr></hr>
+           <p>I have my Mother,Father,Uncle,Aunty & brother in my family.<br></br>I have 7 cats & 3 dogs.</p>
+        </>
+    )
+}
+
+export default Fdha2
