fd3d42797512736042e5360414968fe4f5110760
Rajasree Laha <laharajasree121@gmail.com>
2024-07-07 11:47:51 +0530

    Create head_hass2.jsx

---
+++ resume/head_hass2.jsx
@@ -0,0 +1,12 @@
+//4 comp. in 4 diff files
+import React from "react";
+const Headha2=()=>{
+    return(
+        <>
+            <h2><u>Resume</u></h2>
+        </>
+    )
+}
+
+
+export default Headha2
